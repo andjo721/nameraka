e4744be46bcc25b6e66240ca3a18e0498786a0a6
Andreas Johansson <andjo721@gmail.com>
2023-01-09 00:10:49 +0100

    Added LTO_ENABLE for smaller target binaries.

--- rules.mk
+++ rules.mk
@@ -11,6 +11,8 @@ COMBO_ENABLE = yes
 
 DYNAMIC_TAPPING_TERM_ENABLE = yes
 
+LTO_ENABLE = yes
+
 INTROSPECTION_KEYMAP_C = manna-harbour_miryoku.c # keymaps
 
 include users/manna-harbour_miryoku/custom_rules.mk
