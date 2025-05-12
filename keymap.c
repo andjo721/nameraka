b4f4e6dc77ab0028a4aaec59b3bcc6357bcd2a4b
Andreas Johansson <andreas@Mac-Myra.local>
2025-05-12 18:16:28 +0200

    Make thumb buttons non-chordal
    Also fix position of media layer button in README.

--- keymap.c
+++ keymap.c
@@ -7,5 +7,5 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-                       'L', 'L', 'L',  '*', 'R', 'R'
+                       '*', '*', '*',  '*', '*', '*'
     );
