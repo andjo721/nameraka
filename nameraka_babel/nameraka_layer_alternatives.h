537c9ec918677de0cf4ebb154b95392208a9af9c
Andreas Johansson <andjo721@gmail.com>
2026-03-02 23:00:17 +0100

    Map FUN layer left thumb to Super+§ for in-app window switching

--- nameraka_babel/nameraka_layer_alternatives.h
+++ nameraka_babel/nameraka_layer_alternatives.h
@@ -93,7 +93,7 @@ U_NP,        SE_SECT,  SE_1,   SE_2,   SE_3,   SE_PERC,       U_NA,        KC_ME
 U_NP,       KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR,        MS_BTN1,     OS_STATUS,   U_NA,         U_NA,       U_NA,     U_NP, \
 U_NP,       KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL,        KC_HYPR,     KC_LSFT,     KC_LCTL,      NMK_LALT,   KC_LGUI,  U_NP, \
 U_NP,       KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS,        U_NA,        KC_MEH,      U_NA,         U_NA,       U_NA,     U_NP, \
-                           KC_APP,KC_SPC, KC_TAB,        U_NA,        U_NA,        U_NA
+                           KC_APP,KC_SPC, LGUI(KC_GRV),  U_NA,        U_NA,        U_NA
 
 
 #define NAMERAKA_ALTERNATIVES_EMPTY \
